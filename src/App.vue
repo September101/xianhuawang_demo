b83fb907b95861bf47fe2eae52d8cba2b589e769
anlianming <1367156312@qq.com>
2017-09-25 20:40:00 +0800

    分页路由

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <!-- <img src="./assets/logo.png">
+    <hello></hello> -->
+    <!-- 底部分页菜单 -->
+    <footer class="footer">
+      <ul>
+        <li class="homeli">
+          <router-link to="/home">
+              <span><img src="./pages/imgs/home_icon_click_new.png"></span>
+              <p>首页</p>
+          </router-link>
+        </li>
+        <li class="sortli">
+          <router-link to="/sort">
+              <span><img src="./pages/imgs/classify_icon_new.png"></span>
+              <p>分类</p>
+          </router-link>
+        </li>
+        <li class="cartli">
+          <router-link to="/cart">
+              <span><img src="./pages/imgs/cart_icon_new.png"></span>
+              <p>购物车</p>
+          </router-link>
+        </li>
+        <li class="mineli">
+          <router-link to="/mine">
+              <span><img src="./pages/imgs/my_icon_new.png"></span>
+              <p>我的</p>
+          </router-link>
+        </li>
+      <router-view></router-view>
+      </ul>
+    </footer>
   </div>
 </template>
 
@@ -17,6 +47,8 @@ export default {
 </script>
 
 <style>
+
+/* 页面样式 */
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +56,51 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+/* footer样式 */
+.footer{
+    width: 100%;
+    height: 0.51rem;
+    background-color: rgba(250,250,250,1);
+    border-top: 1px solid #ddd;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+}
+.footer ul{
+    width: 100%;
+    height: 0.51rem;
+    display: flex;
+}
+.footer ul li{
+    width: 25%;
+    height: 0.51rem;
+    flex: 1;
+    float: left;
+    text-align: center;
+}
+.footer li a {
+    color: #494949;
+}
+.footer li span{
+    display: block;
+    width: 0.24rem;
+    height: 0.24rem;
+    margin: 0.04rem auto 0.02rem auto;
+}
+.footer li span img{
+    width: 100%;
+    height: 100%;
+}
+.footer li:nth-child(1) p{
+    color: #fe6600;
+}
+.footer li p{
+    font-size: 0.12rem;
+    text-align: center;
+    width: 100%;
+    height: 0.20rem;
+    line-height: 0.20rem;
+    margin-bottom: 0.03rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,40 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from "vue-router"
 
+// 全局安装路由(让任何组件都可以使用路由)
+Vue.use(VueRouter);
+
+// 分别导入4个组件
+import Home from './pages/Home'
+import Sort from './pages/Sort'
+import Cart from './pages/Cart'
+import Mine from './pages/Mine'
+
+// 配置路由规则
+const routes = [
+  {path:"",component:Home},
+  {path:'/home',component:Home},
+  {path:'/sort',component:Sort},
+  {path:'/cart',component:Cart},
+  {path:'/mine',component:Mine}
+]
+
+// 创建路由实例
+const router = new VueRouter({
+  // 路由规则
+  routes
+})
+
+// 去掉控制台警告
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: { App },
+  // 将路由实例挂载到vue上
+  router
 })
